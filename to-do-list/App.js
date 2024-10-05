4a3646d9d5b1a559baee0a2bd67552530dcd3ced
HashamUrRehman <hashamurrehman01@gmail.com>
2024-10-05 05:15:31 -0700

    Update App.js

--- to-do-list/App.js
+++ to-do-list/App.js
@@ -31,7 +31,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>To-Do List</Text>
+      <Text style={styles.header}>To-Do-List</Text>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
@@ -66,15 +66,85 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 30, backgroundColor: '#fff' },
-  header: { fontSize: 40, fontWeight: 'bold', marginBottom: 20, marginTop: 70, },
-  inputContainer: { flexDirection: 'row', marginBottom: 20 },
-  input: { flex: 1, borderColor: '#ddd', borderWidth: 2, padding: 10, borderRadius: 10 },
-  addButton: { backgroundColor: 'black', padding: 10, borderRadius: 10, marginLeft: 10 },
-  addButtonText: { color: '#fff', fontWeight: 'bold' },
-  taskContainer: { flexDirection: 'row', justifyContent: 'space-between', padding: 10, borderBottomWidth: 1, borderBottomColor: '#ddd' },
-  taskText: { fontSize: 18 },
-  buttonsContainer: { flexDirection: 'row' },
-  editButton: { color: '#007BFF', marginRight: 10 },
-  deleteButton: { color: '#FF0000' },
+container: {
+  flex: 1,
+  padding: 30,
+  backgroundColor: '#F5F5F5', 
+},
+header: {
+  fontSize: 40,
+  fontWeight: 'bold',
+  color: '#333', 
+  marginBottom: 20,
+  marginTop: 60,
+  textAlign: 'center', 
+},
+inputContainer: {
+  flexDirection: 'row',
+  marginBottom: 40,
+  alignItems: 'center', 
+},
+input: {
+  flex: 1,
+  borderColor: '#CCC',
+  borderWidth: 2.5,
+  padding: 15,
+  borderRadius: 12, 
+  backgroundColor: '#FFF',
+  elevation: 2, 
+  shadowColor: '#000',
+  shadowOffset: { width: 0, height: 3 },
+  shadowOpacity: 0.3,
+  shadowRadius: 1,
+},
+addButton: {
+  backgroundColor: '#007BFF', 
+  padding: 15,
+  borderRadius: 10, 
+  marginLeft: 10,
+  elevation: 2, 
+  shadowColor: '#000',
+  shadowOffset: { width: 0, height: 2 },
+  shadowOpacity: 0.3,
+  shadowRadius: 4,
+},
+addButtonText: {
+  color: '#fff',
+  fontWeight: 'bold',
+  fontSize: 18, 
+},
+taskContainer: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center', 
+  padding: 15,
+  marginBottom: 10,
+  backgroundColor: '#FFF',
+  borderRadius: 10,
+  borderWidth: 1,
+  borderColor: '#E5E5E5',
+  elevation: 1, 
+  shadowColor: '#000',
+  shadowOffset: { width: 0, height: 2 },
+  shadowOpacity: 0.5,
+  shadowRadius: 8,
+},
+taskText: {
+  fontSize: 18,
+  color: '#333',
+},
+buttonsContainer: {
+  flexDirection: 'row',
+  alignItems: 'center',
+},
+editButton: {
+  color: '#28A745', 
+  marginRight: 15,
+  fontSize: 16, 
+},
+deleteButton: {
+  color: '#FF0000', 
+  fontSize: 16,
+},
+
 });
